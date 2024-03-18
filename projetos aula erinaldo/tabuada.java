52a68a131cc450d9e9bd63cc0a58e3118fc3a484
portezera <134521956+portezera@users.noreply.github.com>
2024-03-18 09:12:22 -0300

    l

---
+++ projetos aula erinaldo/tabuada.java
@@ -0,0 +1,14 @@
+package condicional;
+
+import java.util.Scanner;
+
+public class tabuada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
