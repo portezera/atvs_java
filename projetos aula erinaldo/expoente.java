51599db79d3a72234048a17663539bf61bc41bde
portezera <134521956+portezera@users.noreply.github.com>
2024-04-01 10:54:44 -0300

    a

--- projetos aula erinaldo/expoente.java
+++ projetos aula erinaldo/expoente.java
@@ -8,7 +8,7 @@ public class potencias {
 		// TODO Auto-generated method stub
 
 		int fim,i,j,x,expoente, numdetermos;
-		int den,denominador,fat,snumerico;
+		int den = 0,denominador,fat,snumerico;
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -34,10 +34,20 @@ public class potencias {
 			expoente = i + i;
 			if ( ( expoente %2 ) ==0 )
             snumerico = snumerico-(int)Math.pow(x,expoente);
-				
+			else 
+				snumerico += (int)Math.pow(x,expoente)/fat;
+			if (denominador == 4)
+				den = -1;
+			if (denominador == 1)
+				den=1;
+			if (den==1)
+				//denominador = denominador + 1
+				denominador += 1;
+			else 
+				denominador -= 1;
+			
+			System.out.printf("%d ",snumerico);
 		}
-		
-		
 		
 		
 	}
