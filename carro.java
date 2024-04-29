11622b6e37498c2d6879aa7f3012bf89eb9acdd8
portezera <134521956+portezera@users.noreply.github.com>
2024-04-29 10:56:29 -0300

    a

---
+++ carro.java
@@ -0,0 +1,35 @@
+package condicional;
+
+import java.util.Scanner;
+
+public class atv8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int carro, preço_final, escolha, escolha1;
+		
+		System.out.println("digite o valor do carro:");
+		carro = sc.nextInt();
+		
+		System.out.println("======================================================================================");
+		System.out.println("A quantidade de parcelas pode ser: 6, 12, 18, 24, 30, 36, 42, 48, 54 e 60 meses");
+		System.out.println("sendo .............................3%..6%..9%..12%.15%.18%.21%.24%.27%..30% de juros");
+		System.out.println("compra à vista tem desconto de 20%");
+		System.out.println("======================================================================================");
+		
+		System.out.println("se quer pagar a vista digite 1 se nao quiser digite 2:");
+		escolha = sc.nextInt();
+		
+		if (escolha = 1) {
+			preco_final = carro - 200%
+		}
+		
+		System.out.println("digite quantas vezes quer parcelar 6, 12, 18, 24, 30, 36, 42, 48, 54 e 60:");
+		escolha1 = sc.nextInt();
+		
+		
+	}
+
+}
